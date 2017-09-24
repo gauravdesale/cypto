7b05f6464c03054cede635cdae33da2b16788d22
gauravdesale <desalegaurav60@gmail.com>
2017-09-24 15:42:20 -0500

    just continuing on the css decryption algorithm

--- cssCracker.c
+++ cssCracker.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <vector>
+#include <string.h>
 int lfsr1[17];
 int lfsr2[25];
+vector<int> resultlfsr;
 int key = "dskhfdhlekljkseflkjlkefljk";
 int main(){
 	lfsr[17] = key[0];
 	lfsr[18] = key[1];
-	for(int i = 0
-
+	for(int i = 0; i <= 3; i++){
+		lfsr1[i] = key[i];
+	}
+	for(int i = key.strlen - 4; i < key.strlen; i++){
+		lfsr2[i] = key[i];
+	}
+	for(int i = 0; i < key.strlen; i++){
+		resultlfsr = lfsr1[i] + lfsr2[i];
+		resultlfsr = resultlfsr % 265; 
+	}
